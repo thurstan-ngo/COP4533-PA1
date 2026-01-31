08d9a4ca4c85728d6d33d40e4daf290e91a6479e
thurstanngo <146325171+thurstan-ngo@users.noreply.github.com>
2026-01-30 21:36:49 -0500

    Create function to read input file

--- matcher_verifier.py
+++ matcher_verifier.py
@@ -20,6 +20,30 @@ def gale_shapley(free_hospitals, hospital_preferences, student_preferences, matc
 
     return unmatched_hospitals
 
+# Read input file
+def read_input(input_file, hospital_preferences, student_preferences):
+    
+    with open(input_file) as f_in:
+        # Read first line of input file
+        first_line = f_in.readline()
+        try:
+            if not first_line:
+                print('File is empty')
+                return -1
+            n = int(first_line)     # n hospitals and n students
+        except:
+            print('First line of input file is invalid')
+            return -1
+        
+        # Initialize hospital_preferences and student_preferences
+        for _ in range(n):
+            hospital_preferences.append(list(f_in.readline().split()))
+
+        for _ in range(n):
+            student_preferences.append(list(f_in.readline().split()))
+
+    return n
+
 # read the matching
 def read_matching(match_file):
     matches = {}
@@ -76,86 +100,63 @@ def is_stable(matches, hospital_preferences, student_preferences):
 
 
 def main():
-    mode = input('Type 1 for matching engine or 2 for verifier: ')
     
-    if mode == '1':
+    while True:
+        mode = input('Type 1 for matching engine, 2 for verifier, or 3 to exit the program: ')
         
-        # Read input
-        file = input('Type name of input file: ')
-        with open(file) as f_in:
-            # Read first line of input file
-            first_line = f_in.readline()
-            try:
-                if not first_line:
-                    print('File is empty')
-                    return
-                n = int(first_line)     # n hospitals and n students
-            except:
-                print('First line of input file is invalid')
-                return
+        hospital_preferences = []       # list of (tuples of strings that have values of 1 to n)
+        student_preferences = []        # list of (tuples of strings that have values of 1 to n)
+
+        if mode == '1':
             
-            hospital_preferences = []       # list of (tuples of strings that have values of 1 to n)
-            for _ in range(n):
-                input_line = f_in.readline()
-                hospital_preferences.append(list(input_line.split()))
-            student_preferences = []        # list of (tuples of strings that have values of 1 to n)
-            for _ in range(n):
-                input_line = f_in.readline()
-                student_preferences.append(list(input_line.split()))     
+            # Read input
+            input_file = input('Type name of input file: ')
+            n = -1
+            while n == -1:
+                n = read_input(input_file, hospital_preferences, student_preferences)
+            
+            
+            # Matching algorithm
+            
+            matches = {}
+            # Key:      (student)  -> string
+            # Value:    (hospital) -> int
+            
+            unmatched_hospitals = list(range(1, n+1))
+            while unmatched_hospitals:
+                unmatched_hospitals = gale_shapley(unmatched_hospitals, hospital_preferences, student_preferences, matches)
 
+            with open('output.txt', 'w', encoding="utf-8") as f_out:
+                for key, value in sorted(matches.items(), key=lambda item: item[1]):
+                    f_out.write(f'{value} {key}\n')
+            
+            print('Output of matching engine was written to output.txt')
 
-        # Matching algorithm
-        
-        matches = {}
-        # Key:      (student)  -> string
-        # Value:    (hospital) -> int
-        
-        unmatched_hospitals = list(range(1, n+1))
-        while unmatched_hospitals:
-            unmatched_hospitals = gale_shapley(unmatched_hospitals, hospital_preferences, student_preferences, matches)
+            
+        elif mode == '2':
+            # Read  input file of preferences
+            input_file = input('Type name of input file: ')
+            n = -1
+            while n == -1:
+                n = read_input(input_file, hospital_preferences, student_preferences)
 
-        with open('output.txt', 'w', encoding="utf-8") as f_out:
-            for key, value in sorted(matches.items(), key=lambda item: item[1]):
-                f_out.write(f'{value} {key}\n')
-        
-        print('Output of matching engine was written to output.txt')
+            # Read output matching file
+            match_file = input('Type name of matching output file: ')
+            matches = read_matching(match_file)
 
-        
-    elif mode == '2':
-        # Read  input file of preferences
-        file = input('Type name of input file: ')
-        with open(file) as f_in:
-            first_line = f_in.readline()
-            try:
-                if not first_line:
-                    print('File is empty')
-                    return
-                n = int(first_line)
-            except:
-                print('First line of input file is invalid')
+            # check valid matching
+            if not is_valid_matching(matches, n):
                 return
 
-            hospital_preferences = []
-            for _ in range(n):
-                hospital_preferences.append(f_in.readline().split())
+            # check stable matching
+            if is_stable(matches, hospital_preferences, student_preferences):
+                print("Valid & Stable")
+        
+        elif mode == '3':
+            break
 
-            student_preferences = []
-            for _ in range(n):
-                student_preferences.append(f_in.readline().split())
-
-        # Read output matching file
-        match_file = input('Type name of matching output file: ')
-        matches = read_matching(match_file)
-
-        # check valid matching
-        if not is_valid_matching(matches, n):
-            return
-
-        # check stable matching
-        if is_stable(matches, hospital_preferences, student_preferences):
-            print("Valid & Stable")
-    else:
-        print('Invalid input')
+        else:
+            print('Invalid input')
             
 
 if __name__ == '__main__':
